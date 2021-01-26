ec5963ebcd6b36572505ed925d5ce8a5d373717d
Slimelvl1 <pedroh.af@hotmail.com>
2021-01-26 14:44:33 -0300

    Criado e removido atributo dinâmico de objetos do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,5 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=20):
+    def __init__(self, *filhos, nome=None, idade=49):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -9,7 +9,7 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-     Pedro = Pessoa(nome='Pedro')
+     Pedro = Pessoa(nome='Pedro',)
      Silvio = Pessoa(Pedro, nome='Silvio')
      print(Pessoa.cumprimentar(Silvio))
      print(id(Silvio))
@@ -17,7 +17,11 @@ if __name__ == '__main__':
      print(Silvio.nome)
      print(Silvio.idade)
      for filho in Silvio.filhos:
-        print(Silvio.nome)
+        print(filho.nome)
+     Silvio.sobrenome = 'Freitas'
+     del Silvio.filhos
+     print(Silvio.__dict__)
+     print(Pedro.__dict__)
 
 
 
